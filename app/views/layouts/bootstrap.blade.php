b016c9a0b53a0524eba8677287eb610a09182d15
Adam Engebretson <adam@enge.me>
2013-11-19 13:23:12 -0600

    Add goSquared
    Signed-off-by: Adam Engebretson <adam@enge.me>

--- app/views/layouts/bootstrap.blade.php
+++ app/views/layouts/bootstrap.blade.php
@@ -110,5 +110,14 @@
     </script>
 
     @yield('scripts')
+
+    <script>
+      !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+      arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+      d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+      insertBefore(d,q)}(window,document,'script','_gs');
+      
+      _gs('GSN-675110-Q');
+    </script>
   </body>
 </html>
